91d980ae18866ba548de5712bb5f560bcb5995e7
stefanoluzi <stefanoluzi@hotmail.com>
2023-12-26 19:50:17 -0300

    First commit proyect

---
+++ src/main/java/dev/stefanoluzi/movies/Repository/MovieRepository.java
@@ -0,0 +1,13 @@
+package dev.stefanoluzi.movies.Repository;
+
+import dev.stefanoluzi.movies.model.Movie;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findMovieByimdbId(String imdbId);
+}
---
+++ src/main/java/dev/stefanoluzi/movies/service/MovieService.java
@@ -0,0 +1,26 @@
+package dev.stefanoluzi.movies.service;
+
+import dev.stefanoluzi.movies.Repository.MovieRepository;
+import dev.stefanoluzi.movies.model.Movie;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+    public List<Movie> allMovies(){
+        return movieRepository.findAll();
+    }
+    public Optional<Movie> singleMovie(String imdbId){
+        return movieRepository.findMovieByimdbId(imdbId);
+    }
+
+
+
+}
